e065595961d91b6bd95ffbeeec5b81a0d3ebc271
husni-robani <husnir2005@gmail.com>
2024-04-22 21:55:04 +0700

    add study program column

--- backend/models.py
+++ backend/models.py
@@ -5,11 +5,13 @@ class Journal(db.Model):
     title = db.Column(db.String(80),  nullable=False, unique=False)
     author = db.Column(db.String(80), nullable=False, unique=False)
     abstract = db.Column(db.Text, nullable=False, unique=False)
+    study_program = db.Column(db.Enum("Akuntansi", "Manajemen", "Teknik Informatika", "Teknik Industri", "Sistem Informasi"), nullable=False)
 
     def to_json(self):
         return {
             'id': self.id,
             'title': self.title,
             'author': self.author,
-            'abstract': self.abstract
+            'abstract': self.abstract,
+            'study_program': self.study_program
         }
