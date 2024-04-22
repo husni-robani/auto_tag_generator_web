03b232682059bb3383d61f369603903237f8a2fe
husni-robani <husnir2005@gmail.com>
2024-04-22 21:07:37 +0700

    change page component on route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
-import Layout from "./Layout.jsx";
-import InputJournal from "./Pages/InputJournal.jsx";
+import NewJournal from "./Pages/NewJournal.jsx";
 
 const router = createBrowserRouter([
   {
@@ -14,7 +12,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/input-journal",
-    element: <InputJournal />,
+    element: <NewJournal />,
   },
 ]);
 
