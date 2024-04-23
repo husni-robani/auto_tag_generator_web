6f01436d89b43385495f0ed869ad67fb4c25cf69
husni-robani <husnir2005@gmail.com>
2024-04-23 17:27:18 +0700

    change navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useMatch, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const navigationClass = (path) => {
+    return (
+      "block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent " +
+      (useMatch(path) ? "md:text-blue-700" : "md:text-slate-400")
+    );
+  };
+
+  const handleNavigation = (path) => (e) => {
+    e.preventDefault();
+    navigate(path);
+  };
   return (
     <>
       <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
@@ -43,35 +57,22 @@ export default function Navbar() {
             <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
               <li>
                 <a
-                  href="#"
-                  class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  onClick={handleNavigation("/")}
+                  href="/"
+                  class={navigationClass("/")}
                   aria-current="page"
                 >
-                  Home
+                  Journal List
                 </a>
               </li>
               <li>
                 <a
-                  href="#"
-                  class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  onClick={handleNavigation("/input-journal")}
+                  href="/input-journal"
+                  class={navigationClass("/input-journal")}
+                  aria-current="page"
                 >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Contact
+                  Input Journal
                 </a>
               </li>
             </ul>
