46b9de84908e92a527b019de943d928e58aa630c
husni-robani <husnir2005@gmail.com>
2024-04-23 17:26:05 +0700

    clean config.py

--- backend/config.py
+++ backend/config.py
@@ -7,16 +7,6 @@ app = Flask(__name__)
 CORS(app=app)
 
 # setup database
-
-#  without SQLAlchemy
-# app.config["MYSQL_HOST"] = "localhost"
-# app.config["MYSQL_USER"] = "root"
-# app.config["MYSQL_PASSWORD"] = "12345678"
-# app.config["MYSQL_DB"] = "auto_tag_web_db"
-
-# mysql = MySQL(app=app)
-
-# with SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:12345678@127.0.0.1:3306/auto_tag_web_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app=app)
