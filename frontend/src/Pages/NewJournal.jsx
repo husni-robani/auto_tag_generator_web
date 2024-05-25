070020bb3bf7cff11bf85875e6d3c10887ed977d
husni-robani <husnir2005@gmail.com>
2024-05-25 18:28:34 +0700

    remove predicting study program, predicting study program move to backend

--- frontend/src/Pages/NewJournal.jsx
+++ frontend/src/Pages/NewJournal.jsx
@@ -2,56 +2,37 @@ import Layout from "../Layout";
 import JournalForm from "../components/JournalForm";
 
 export default function NewJournal({ model_api_url, web_api_url }) {
-  const handleSubmit = async (inputValues, file) => {
-    const data = new FormData();
-    data.append("author", inputValues.author);
-    data.append("title", inputValues.title);
-    data.append("abstract", inputValues.abstract);
-    data.append("file", file);
-
-    // predict study program
-    const study_program = await predict_study_program([data.get("abstract")]);
-    data.append("study_program", study_program);
-
-    store_journal(data);
-  };
-
-  const predict_study_program = async (abstract) => {
-    const url = model_api_url + "/api/predict";
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        abstract,
-      }),
-    };
-
-    const response = await fetch(url, options);
-
-    if (response.status != "200") {
-      const errors = await response.json();
-      alert(errors.message);
-    } else {
-      const respone_json = await response.json();
-      return respone_json.results;
+  const handleSubmit = async (inputValues) => {
+    const formDatas = [];
+    for (let index in inputValues) {
+      const data = new FormData();
+      data.append("author", inputValues[index].author);
+      data.append("title", inputValues[index].title);
+      data.append("abstract", inputValues[index].abstract);
+      data.append("file", inputValues[index].file);
+      formDatas.push(data);
     }
+    store_journal(formDatas);
   };
 
   const store_journal = async (data) => {
     const url = web_api_url + "/api/store_journal";
-    const options = {
-      method: "POST",
-      body: data,
-    };
-    const response = await fetch(url, options);
 
-    if (response.status != "201") {
-      const errors = await response.json();
-      alert(errors.message);
-    } else {
-      // updateCallback(); // undefined function
+    // store data one by one
+    for (let index in data) {
+      const options = {
+        method: "POST",
+        body: data[index],
+      };
+
+      const response = await fetch(url, options);
+
+      if (response.status != "201") {
+        const errors = await response.json();
+        alert(errors.message);
+      } else {
+        // updateCallback(); // undefined function
+      }
     }
   };
 
